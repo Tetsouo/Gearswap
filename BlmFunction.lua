cd1e6abb40dd0f973aafa7d410f29036de4ea24d
Eric Jouanchicot <ejouanchicot@gmail.com>
2023-07-10 06:45:35 +0200

    Test de commit

--- BlmFunction.lua
+++ BlmFunction.lua
@@ -1,3 +1,4 @@
+--commentaire de test voir si le commmit a lieu 
 function BuffSelf(spell, eventArgs)
     local allRecasts = windower.ffxi.get_spell_recasts()
     local PhalanxCD = allRecasts[106]
